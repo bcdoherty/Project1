b28415ea47c91b2b92f5e862e04c26171308b211
maureenlangley <maureenlangley7@gmail.com>
2018-10-15 15:39:28 -0400

    part 1 done

--- project1-206.py
+++ project1-206.py
@@ -6,7 +6,29 @@ from datetime import date
 
 def getData(file):
 # get a list of dictionary objects from the file
-#Input: file name
+	file = open(file, 'r') #Input: file name
+	lines = file.readlines()
+	file.close()
+
+	dictList = []
+
+	for line in lines:
+		dic = {}
+
+		values = line.split(":")
+		first = values[0]
+		last = values[1]
+		email = values[2]
+		year = values[3]
+		DOB = values[4]
+
+		dic["first"] = first
+		dic["last"] = last
+		dic["email"] = email
+		dic["class"] = year
+		dic["DOB"] = DOB
+		dictList.append(dic)
+	return dictList
 #Ouput: return a list of dictionary objects where
 #the keys are from the first row in the data. and the values are each of the other rows
 
